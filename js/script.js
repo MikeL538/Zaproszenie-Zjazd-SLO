9909d149ff9463b021139319b5e60057ceae9ff4
MikeL538 <mikel538.work@gmail.com>
2026-01-16 15:26:21 +0100

    feat: split name and surname inputs, add validations and minor styling - Split full name into separate 'name' and 'surname' inputs for easier list filtering - Added 'surname' column in Supabase - Added RLS/Check constraints:   - min 3 and max 66 characters for name, surname, email   - max 255 characters for add_info   - graduation between 1950 and 2025 - Minor form styling adjustments (text-align: center)

--- js/script.js
+++ js/script.js
@@ -5,10 +5,11 @@ const supabaseKey = "sb_publishable_kSmSt52th8XAYGbce3CtwA_uIdN8fKL";
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-async function saveGuest(guestName, eMail, gradYear, addInfo) {
+async function saveGuest(guestName, guestSurname, eMail, gradYear, addInfo) {
   const { data, error } = await supabase.from("guest_data").insert([
     {
       name: guestName,
+      surname: guestSurname,
       e_mail: eMail,
       graduation: gradYear,
       add_info: addInfo,
@@ -24,14 +25,27 @@ async function saveGuest(guestName, eMail, gradYear, addInfo) {
   alert("Zapisano!");
 }
 
+// HANDLER FOR register form request
 const btnGuest = document.querySelector("#btnGuest");
-
 btnGuest.addEventListener("click", (e) => {
   e.preventDefault();
   const guestName = document.querySelector("#name").value;
+  const guestSurname = document.querySelector("#surname").value;
   const email = document.querySelector("#email").value;
   const year = document.querySelector("#year").value;
   const message = document.querySelector("#message").value;
 
-  saveGuest(guestName, email, year, message);
+  saveGuest(guestName, guestSurname, email, year, message);
 });
+
+// PREVENTS / REQUIRES
+// Max signs for inputs and textareas
+function setMaxSigns() {
+  const guestName = document.querySelector("#name").length;
+  const guestSurname = document.querySelector("#surname");
+  const email = document.querySelector("#email");
+  const year = document.querySelector("#year");
+  const message = document.querySelector("#message");
+
+  console.lop(guestName);
+}
